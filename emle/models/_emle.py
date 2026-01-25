6b0d99078d6175f3d653e6cafcefa8ed4d0e00ee
Joao Morado <joaomorado@gmail.com>
2026-01-25 21:24:55 +0000

    Revert dynamic MM

--- emle/models/_emle.py
+++ emle/models/_emle.py
@@ -794,20 +794,17 @@ class EMLE(_torch.nn.Module):
             q_core_mm = self._charges_mm
 
         # Compute the dispersion or LJ energy.
-        if self._method in ["electrostatic"] and self._dispersion_mode:
-            sigma_mm = self._lj_sigma_emle[nagl_rows, nagl_cols]
-            epsilon_mm = self._lj_eps_emle[nagl_rows, nagl_cols]
-            #q_core_mm = self._q_core_emle[nagl_rows, nagl_cols] + self._q_val_emle[nagl_rows, nagl_cols]
-            #q_val_mm = None
-            alpha_qm = self._emle_base.get_isotropic_polarizabilities_thole(A_thole)
-        elif self._method in ["nonpol", "mm"] and self._dispersion_mode:
+        #if self._method in ["electrostatic"] and self._dispersion_mode:
+        #    sigma_mm = self._lj_sigma_emle[nagl_rows, nagl_cols]
+        #    epsilon_mm = self._lj_eps_emle[nagl_rows, nagl_cols]
+        #    #q_core_mm = self._q_core_emle[nagl_rows, nagl_cols] + self._q_val_emle[nagl_rows, nagl_cols]
+        #    #q_val_mm = None
+        #    alpha_qm = self._emle_base.get_isotropic_polarizabilities_thole(A_thole)
+        if self._method in ["electrostatic", "nonpol", "mm"] and self._dispersion_mode:
             sigma_mm = self._emle_base._lj_sigma_mm[nagl_rows, nagl_cols]
             epsilon_mm = self._emle_base._lj_eps_mm[nagl_rows, nagl_cols]
             if self._method != "mm":
                 alpha_qm = self._emle_base.get_isotropic_polarizabilities_thole(A_thole)
-                # alpha_qm = self._emle_base.get_isotropic_polarizabilities_xdm(
-                #    self._atomic_numbers, -60 * q_val * s**3
-                # )
             else:
                 alpha_qm = None
         else:
