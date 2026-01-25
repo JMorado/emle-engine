49518770e64f51c91a156107652c2d347781fce0
Joao Morado <joaomorado@gmail.com>
2026-01-25 13:12:07 +0000

    Only dynamic LJ

--- emle/models/_emle.py
+++ emle/models/_emle.py
@@ -355,7 +355,6 @@ class EMLE(_torch.nn.Module):
         else:
             q_core_mm = _torch.empty(0, dtype=dtype, device=device)
 
-        print("NAGL params:", nagl_params is not None)
         # Extract MM LJ parameters for QM atoms if available
         if nagl_params is not None and method == "mm":
             epsilon_mm_qm = _torch.tensor(
@@ -798,14 +797,9 @@ class EMLE(_torch.nn.Module):
         if self._method in ["electrostatic"] and self._dispersion_mode:
             sigma_mm = self._lj_sigma_emle[nagl_rows, nagl_cols]
             epsilon_mm = self._lj_eps_emle[nagl_rows, nagl_cols]
-            q_core_mm = self._q_core_emle[nagl_rows, nagl_cols] + self._q_val_emle[nagl_rows, nagl_cols]
-            q_val_mm = None
+            #q_core_mm = self._q_core_emle[nagl_rows, nagl_cols] + self._q_val_emle[nagl_rows, nagl_cols]
+            #q_val_mm = None
             alpha_qm = self._emle_base.get_isotropic_polarizabilities_thole(A_thole)
-
-            print("Using EMLE NAGL parameters for dispersion calculation.")
-            print("sigma_mm:", sigma_mm)
-            print("epsilon_mm:", epsilon_mm * HARTREE_TO_KCALMOL * 4.184)
-            print("q_core_mm:", q_core_mm)
         elif self._method in ["nonpol", "mm"] and self._dispersion_mode:
             sigma_mm = self._emle_base._lj_sigma_mm[nagl_rows, nagl_cols]
             epsilon_mm = self._emle_base._lj_eps_mm[nagl_rows, nagl_cols]
