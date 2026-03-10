471631f5d29723fcbb34f1fdb9353ca89066d829
Lester Hedges <lester.hedges@gmail.com>
2026-03-10 16:51:39 +0000

    Add support for TorchANI 2.0.

--- emle/models/_emle.py
+++ emle/models/_emle.py
@@ -40,16 +40,8 @@ from . import EMLEBase as _EMLEBase
 from . import _patches
 from ._utils import _apply_switching_function, _preprocess_coordinates
 
-# Monkey-patch the TorchANI BuiltInModel and BuiltinEnsemble classes so that
-# they call self.aev_computer using args only to allow forward hooks to work
-# with TorchScript.
-_torchani.models.BuiltinModel = _patches.BuiltinModel
-_torchani.models.BuiltinEnsemble = _patches.BuiltinEnsemble
-
 try:
     import NNPOps as _NNPOps
-
-    _NNPOps.OptimizedTorchANI = _patches.OptimizedTorchANI
 
     _has_nnpops = True
 except:
